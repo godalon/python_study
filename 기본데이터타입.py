1e2f3c35aa25f2392244351b8d6e2ba2d5a6accb
DAHAM <hamk1@naver.com>
2021-11-09 12:28:57 +0900

    1109

---
+++ 기본데이터타입.py
@@ -0,0 +1,24 @@
+# 파이썬 기본 데이터 타입
+a = 7
+b = 7.7777
+c = True
+d = False
+e = "문자열"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(2/4)
+print(type(2/4))
+
+#제곱과 나누기
+print(2**3) #2*2*2
+print(5//2) # 2몫
+print(5%2) # 나머지
+
+# 반올림 round()(숫자,자릿수)
+print(round(3.156,2))
+
